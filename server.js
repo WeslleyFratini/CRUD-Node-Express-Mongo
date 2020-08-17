cc2a0607ad748582807082434d58bb41964d0166
WeslleyFratini <weeslleey354@gmail.com>
2020-08-17 17:35:57 -0300

    read

--- server.js
+++ server.js
@@ -8,86 +8,137 @@
 
  // Configurar o Setup da aplicação:
 
- //Chamada dos pacotes:
- var express = require('express');
- var app = express();
- var bodyParser = require('body-parser');
- var mongoose = require('mongoose');
- var Produto = require('./app/models/produto');
+//Chamadas dos pacotes:
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Produto = require('./app/models/produto');
+mongoose.Promise = global.Promise;
 
- //Conexão mongo
+//URI: MLab
+mongoose.connect('mongodb+srv://dbWeslley:weslley123@cluster0.w1tfi.mongodb.net/test', {
+    useMongoClient: true
+});
 
- mongoose.connect('mongodb://dbWeslley:weslley123@cluster0.w1tfi.azure.mongodb.net/dbWeslley?retryWrites=true&w=majority'
-    {
-      useMongoClient:true
-    });
- //mongoose.connect('mongodb://localhost/crud-teste'
- //  {
- //  useMongoClient:true
- //  });
+//Maneira Local: MongoDb:
+/*mongoose.connect('mongodb://localhost:27017/node-crud-api', {
+    useMongoClient: true
+});*/
 
- //Configuração da variável app para usar o 'bodyParser():
- app.use(bodyParser.urlencoded({ extended: true}));
- app.use(bodyParser.json);
+//Configuração da variável app para usar o 'bodyParser()':
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//Definindo a porta onde será executada a nossa api:
+var port = process.env.port || 8000;
 
- //Definindo a porta que API será executada:
- var port = process.env.port || 8000;
+//Rotas da nossa API:
+//=============================================================================
 
- //Rotas da nossa API:
- //==============================================
+//Criando uma instância das Rotas via Express:
+var router = express.Router();
 
+//Middleware para usar em todos os requests enviados para a nossa API- Mensagem Padrão:
+router.use(function(req, res, next) {
+    console.log('Algo está acontecendo aqui....');
+    next(); //aqui é para sinalizar de que prosseguiremos para a próxima rota. E que não irá parar por aqui!!!
+});
 
- //Criando uma instancia das rotas via Express:
- var router = express.Router();
-
- router.use(function(req, res, next){
-    console.log('Algo esta acontecendo aqui...')
-    next();
+//Rota de Teste para sabermos se tudo está realmente funcionando (acessar através: GET: http://localhost:8000/api): 
+router.get('/', function(req, res) {
+    res.json({ message: 'Beleza! Bem vindo(a) a nossa Loja XYZ' })
 });
 
 //API's:
-//=====================================================
+//==============================================================================
 
-//Rotas que terminarem com '/produtos' (servir para GET ALL & POST)
-router.route('./produtos')
+//Rotas que terminarem com '/produtos' (servir: GET ALL & POST)
+router.route('/produtos')
 
-/* 1 - Método : Criar produto (acessar em POST http:localhost:8000/api/produtos)*/
-.post( function (req, res){
-    var produto = new Produto();
+    /* 1) Método: Criar Produto (acessar em: POST http://localhost:8000/api/produtos)*/
+    .post(function(req, res) {
+        var produto = new Produto();
 
-    //Setar campos produtos - REQUEST
-    produto.nome = req.body.nome;
-    produto.preco = req.body.preco;
-    produto.descricao = req.body.descricao;
+        //Aqui vamos setar os campos do produto (via request):
+        produto.nome = req.body.nome;
+        produto.preco = req.body.preco;
+        produto.descricao = req.body.descricao;
 
-    produto.save(function(error){
-        if(error)
-            res.send('Erro ao tentar salvar o produto.....: ' + error);
+        produto.save(function(error) {
+            if(error)
+                res.send('Erro ao tentar salvar o Produto....: ' + error);
+            
+            res.json({ message: 'Produto Cadastrado com Sucesso!' });
+        });
+    })
 
-        res.json({ message: 'Produto cadastrado com sucesso" '});
+    /* 2) Método: Selecionar Todos Produtos (acessar em: GET http://localhost:8000/api/produtos)  */
+    .get(function(req, res) {
+        Produto.find(function(error, produtos) {
+            if(error) 
+                res.send('Erro ao tentar Selecionar Todos os produtos...: ' + error);
+
+            res.json(produtos);
+        });
     });
-})
 
-/* 2 - Método : Selecionar produto (acessar em GET http:localhost:8000/api/produtos)*/
+    //Rotas que irão terminar em '/produtos/:produto_id' (servir tanto para: GET, PUT & DELETE: id):
+    router.route('/produtos/:produto_id')
 
-.get(function(req, res){
-    Produto.find(function(error, produtos){
-        if(error)
-            res.send('Erro ao tentar selecionar todos os produtos...: ' + error);
+    /* 3) Método: Selecionar por Id: (acessar em: GET http://localhost:8000/api/produtos/:produto_id) */
+    .get(function (req, res) {
+        
+        //Função para poder Selecionar um determinado produto por ID - irá verificar se caso não encontrar um detemrinado
+        //produto pelo id... retorna uma mensagem de error:
+        Produto.findById(req.params.produto_id, function(error, produto) {
+            if(error)
+                res.send('Id do Produto não encontrado....: ' + error);
 
-        res.json(produtos);
-    });
-});
+            res.json(produto);
+        });
+    })
 
- //Rota de exemplo:
- router.get('/', function(req, res) {
-    res.json({ message: 'Muito bom! Bem vindo a nossa loja'})
- }); 
+    /* 4) Método: Atualizar por Id: (acessar em: PUT http://localhost:8000/api/produtos/:produto_id) */
+    .put(function(req, res) {
 
- //Definindo uma padrão das rotas prefixadas: '/api:
- app.use('/api', router);
+        //Primeiro: para atualizarmos, precisamos primeiro achar 'Id' do 'Produto':
+        Produto.findById(req.params.produto_id, function(error, produto) {
+            if (error) 
+                res.send("Id do Produto não encontrado....: " + error);
 
-//Iniciando o servidor:
- app.listen(port);
- console.log("Iniciando a app na porta: " + port);
+                //Segundo: 
+                produto.nome = req.body.nome;
+                produto.preco = req.body.preco;
+                produto.descricao = req.body.descricao;
+
+                //Terceiro: Agora que já atualizamos os dados, vamos salvar as propriedades:
+                produto.save(function(error) {
+                    if(error)
+                        res.send('Erro ao atualizar o produto....: ' + error);
+
+                    res.json({ message: 'Produto atualizado com sucesso!' });
+                });
+            });
+        })
+
+        /* 5) Método: Excluir por Id (acessar: http://localhost:8000/api/produtos/:produto_id) */
+        .delete(function(req, res) {
+            
+            Produto.remove({
+                _id: req.params.produto_id
+                }, function(error) {
+                    if (error) 
+                        res.send("Id do Produto não encontrado....: " + error);
+
+                    res.json({ message: 'Produto Excluído com Sucesso!' });
+                });
+            });
+
+
+//Definindo um padrão das rotas prefixadas: '/api':
+app.use('/api', router);
+
+//Iniciando a Aplicação (servidor):
+app.listen(port);
+console.log("Iniciando a app na porta " + port);
